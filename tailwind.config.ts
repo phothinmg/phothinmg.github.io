d68a0114a397d577888c6ee0fc45d75025dc6313
phothinmg <phothinmg@gmail.com>
2024-12-04 00:25:13 +0630

    modified:   README.md

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,14 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx,md}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx,md}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx,md}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 } satisfies Config;
